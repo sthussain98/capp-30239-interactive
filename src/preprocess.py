17a424f09fe45484f75f45f261ebd6f8d3a1ad06
sthussain98 <sthussain@uchicago.edu>
2025-12-03 16:43:47 -0600

    updates

--- src/preprocess.py
+++ src/preprocess.py
@@ -98,7 +98,7 @@ def gen_geojson(df):
     with open('../data/aqi_lhe_data.geojson', 'w') as f:
         geojson.dump(geojson_obj, f)
 
-gen_geojson(clean_aqi())
+#gen_geojson(clean_aqi())
 
 
 # clean the sectoral emissions 
@@ -108,6 +108,13 @@ def clean_sector_emissions():
     sectoral_emissions = pl.read_csv("../data/sectoral_data_lhe.csv")
     sectoral_emissions_fil = sectoral_emissions.filter(pl.col('pollutant')
                                                        .is_in(pol_lst))
+    
+    sectoral_emissions_fil = sectoral_emissions_fil.with_columns(
+    pl.when(pl.col('source') == "Charcoal").then(pl.lit("Coal"))
+     .when(pl.col('source') == "NG").then(pl.lit("Natural Gas"))
+     .when(pl.col('source') == "Gas").then(pl.lit("Natural Gas"))
+     .otherwise(pl.col('source'))
+     .alias("source"))
     
     return sectoral_emissions_fil
 
@@ -137,6 +144,29 @@ def sector_source_pollutant_aggregates(df):
 
     )
     return df_sector_source
+
+def sector_source_aggregates(df):
+    sector_source_agg = \
+    df.group_by(['sector', 'source']).agg(
+        pl.col('emissions').sum().alias('total_emissions'))
+    return sector_source_agg
+
+def source_pollutant_aggregates(df):
+    source_pollutant_agg = \
+    df.group_by(['source', 'pollutant']).agg(
+        pl.col('emissions').sum().alias('total_emissions'))
+    return source_pollutant_agg 
+
+
+
+# df = sector_source_pollutant_aggregates(clean_sector_emissions())
+sector_source = sector_source_aggregates(clean_sector_emissions())
+sector_source.write_csv('../data/sector_source_aggregates.csv')
+
+source_pollutant = source_pollutant_aggregates(clean_sector_emissions())
+source_pollutant.write_csv('../data/source_pollutant_aggregates.csv')
+# #df.write_csv('sector_source_pollutant_aggregates.csv')
+
 
 
 #print(sector_source_pollutant_aggregates(clean_sector_emissions()).head(20))
